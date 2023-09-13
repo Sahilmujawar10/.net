a21696d823529516e7400f84cac61b6d48a53ed8
Sahilmujawar10 <143106795+Sahilmujawar10@users.noreply.github.com>
2023-09-13 16:29:25 +0530

    Add files via upload

---
+++ Assignment 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee("rushikesh", 20000, 1);
+            e1.Display();
+            Console.WriteLine();
+
+            Employee e2 = new Employee("rohit", 25000);
+            e2.Display();
+            Console.WriteLine();
+
+            Employee e3 = new Employee("rahul", 102);
+            e3.Display();
+            Console.WriteLine();
+
+            Employee e4 = new Employee("ravi");
+            e4.Display();
+            Console.WriteLine();
+
+            Employee e5 = new Employee();
+            e5.Display();
+            Console.WriteLine();
+
+            Console.WriteLine(e1.EmpNo);
+            Console.WriteLine(e2.EmpNo);
+            Console.WriteLine(e3.EmpNo);
+            Console.WriteLine(e4.EmpNo);
+            Console.WriteLine(e5.EmpNo);
+            Console.WriteLine(e5.EmpNo);
+            Console.WriteLine(e4.EmpNo);
+            Console.WriteLine(e3.EmpNo);
+            Console.WriteLine(e2.EmpNo);
+            Console.WriteLine(e1.EmpNo);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Employee
+    {
+        private string name;
+        private int empNo = count;
+        private decimal basic;
+        private short deptNo;
+        public static int count = 1;
+        public string Name
+        {
+            set
+            {
+                if (!(value == ""))
+                {
+                    name = value;
+                }
+                else
+                    Console.WriteLine("Blank Space Not Allowed ");
+            }
+            get
+            {
+                return name;
+            }
+
+        }
+        public int EmpNo
+        {
+            get
+            {
+                return empNo;
+            }
+        }
+        public decimal Basic
+        {
+            set
+            {
+                if (value > 10000 && value < 30000)
+                    basic = value;
+                else
+                    Console.WriteLine("Basic must be within range");
+            }
+            get
+            {
+                return basic;
+            }
+        }
+        public short Deptno
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    deptNo = value;
+                }
+                else
+                    Console.WriteLine("DeptNo must be greter than zero");
+            }
+            get
+            {
+                return deptNo;
+            }
+        }
+
+        public Employee(string Name = "", decimal Basic = 0, short Deptno = 1)
+        {
+            this.Name = Name;
+            this.Basic = Basic;
+            this.Deptno = Deptno;
+            count++;
+        }
+
+        public void CalSalary()
+        {
+            int hra = (int)((int)Basic * 0.4);
+            Console.WriteLine("NetSalary: " + (Basic + hra));
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("name : " + name);
+            Console.WriteLine("empNo : " + empNo);
+            Console.WriteLine("basic : " + basic);
+            CalSalary();
+            Console.WriteLine("deptNo: " + deptNo);
+
+        }
+
+    }
+}
---
+++ Assignment 1/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Employee em = new Employee();
+            Console.WriteLine("With wrong value");
+            em.Empno = -10;
+            em.Name = "";
+            em.Salary = 1000;
+            em.Deptno = -5;
+            Console.WriteLine("Gross salary is: " + em.grsssalary());
+            //Console.WriteLine(em.Empno);
+            //Console.WriteLine(em.Name);
+            //Console.WriteLine(em.Salary);
+            //Console.WriteLine(em.Deptno);
+
+
+            Console.WriteLine("--------*---------");
+
+            Console.WriteLine("With right value");
+
+
+            Employee em1 = new Employee();
+            em1.Empno = 10;
+            em1.Name = "Rohitt";
+            em1.Salary = 25000;
+            em1.Deptno = 5;
+            em1.grsssalary();
+            Console.WriteLine("Gross salary is: " + em1.grsssalary());
+            //Console.WriteLine(em1.Empno);
+            //Console.WriteLine(em1.Name);
+            //Console.WriteLine(em1.Salary);
+            //Console.WriteLine(em1.Deptno);
+
+
+
+        }
+    }
+
+    class Employee
+    {
+        private string name;
+        private int empno;
+        private double salary;
+        private int deptno;
+        //private double grosssalary;
+        public int Empno
+        {
+            get
+            {
+                return empno;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    empno = value;
+                    Console.WriteLine("Employee no is: " + value);
+                }
+                else
+                {
+                    Console.WriteLine("Enter greater than 0");
+                }
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if ((!(value.Length > 0)) && (!(value.Contains(" "))))
+                {
+                    Console.WriteLine("Employee name should not conatain Space or Employee name should not blank");
+                }
+                else
+                {
+                    name = value;
+                    Console.WriteLine("Employee name is: " + value);
+                }
+            }
+        }
+
+        public double Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                if ((value > 10000) && (value < 50000))
+                {
+                    salary = value;
+                    Console.WriteLine("Employee salary is: " + value);
+                }
+                else
+                {
+                    Console.WriteLine("Salary must be in range 10000 and 50000");
+                }
+            }
+        }
+
+        public int Deptno
+        {
+            get
+            {
+                return deptno;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    deptno = value;
+                    Console.WriteLine("Employee Department is: " + value);
+                }
+                else
+                {
+                    Console.WriteLine("Enter Department greater than 0");
+                }
+            }
+        }
+
+
+        public double grsssalary()
+        {
+            return ((salary * 0.4) + salary);
+        }
+
+
+
+    }
+
+}
+
